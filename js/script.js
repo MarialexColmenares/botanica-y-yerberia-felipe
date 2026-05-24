346d49ae868a74da8b7d59315a1dd80cf9339b23
Mariallex <marialexcolmenares5@gmail.com>
2026-05-24 09:48:50 -0400

    Pagina Estatica para la yerberia Felipe

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Amarres de Amor
+document.getElementById('abrir-modal-con').onclick = function() {
+    document.getElementById('modal-con').showModal();
+};
+document.getElementById('cerrar-modal-con').onclick = function() {
+    document.getElementById('modal-con').close();
+};
+
+// Lectura del Tarot
+document.getElementById('abrir-modal-der').onclick = function() {
+    document.getElementById('modal-der').showModal();
+};
+document.getElementById('cerrar-modal-der').onclick = function() {
+    document.getElementById('modal-der').close();
+};
+
+// Carta Ancestral
+document.getElementById('abrir-modal-adm').onclick = function() {
+    document.getElementById('modal-adm').showModal();
+};
+document.getElementById('cerrar-modal-adm').onclick = function() {
+    document.getElementById('modal-adm').close();
+};
+
